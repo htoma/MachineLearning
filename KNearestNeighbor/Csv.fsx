e6e260418e16f26011316a8e3bc2f5d5cd81537d
Horia Toma <toma.horia@gmail.com>
2017-02-06 20:59:28 +0100

    Got rid of csv provider and parsing files directly

---
+++ KNearestNeighbor/Csv.fsx
@@ -0,0 +1,10 @@
+﻿#r "bin/Debug/KNearestNeighbor.dll"
+
+open Knn
+open System
+open System.IO
+
+let filename = Path.Combine(__SOURCE_DIRECTORY__, "../Data/train.csv")
+let data,labels = loadData filename
+
+//load test data and compute result
---
+++ KNearestNeighbor/Knn.fs
@@ -0,0 +1,46 @@
+﻿module Knn
+
+open System
+open System.IO
+open FSharp.Charting
+
+let display (points: float[][]) (labels: string[]) =
+    let data = points
+                |> Array.map (fun p -> p.[0],p.[1])
+                |> Array.zip labels
+
+    let uniqueLabels = Seq.distinct labels
+    
+    Chart.Combine(
+        [ for label in uniqueLabels ->
+                let data = data
+                            |> Array.filter (fun e  -> label = fst e)
+                            |> Array.map snd
+                Chart.Point(data).WithDataPointLabels(label)
+        ])
+
+let distanceEuclid (v1: float[]) (v2: float[]) =
+    Array.fold2 (fun s e1 e2 -> s+Math.Pow((e1-e2), 2.0)) 0.0 v1 v2
+    |> sqrt
+
+let classify point data labels k =
+    Array.zip data labels
+    |> Array.map (fun (v,l) -> distanceEuclid v point,l)
+    |> Array.sortBy fst
+    |> Array.take k
+    |> Array.countBy snd
+    |> Array.sortByDescending fst
+    |> Array.map fst
+    |> Array.head
+
+let loadData filename =
+    let content = File.ReadAllLines(filename)
+    
+    let converted = content
+                     |> Array.skip 1
+                     |> Array.map (fun l -> l.Split([|','|]))
+                     |> Array.map (fun e -> e.[0],
+                                            e.[1..]
+                                            |> Array.map (fun s -> s |> Convert.ToDouble))
+
+    converted |> Array.map snd, converted |> Array.map fst
